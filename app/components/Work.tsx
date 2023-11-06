932a59749197c7db26967553b7876a85e4eadbd2
dzyrick2 <daniel.gayao7@gmail.com>
2023-11-06 23:36:30 +0800

    Finished work component

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -6,36 +6,38 @@ export default function Work() {
         <h4 className="font-light">Site that i created.</h4>
       </div>
       <div className="pt-5">
-        <div className="flex items-center justify-between border-t border-gray-900 py-5">
+        <div className="flex items-center justify-between border-t border-gray-900 py-10">
           <h5 className="text-7xl">Inked2600</h5>
           <div className="flex items-center gap-32">
             <span className="text-2xl font-light">Design & Development</span>
             <FiArrowUpRight size={40} />
           </div>
         </div>
-        <div className="flex items-center justify-between border-t border-gray-900 py-5">
+        <div className="flex items-center justify-between border-t border-gray-900 py-10">
           <h5 className="text-7xl">Emp</h5>
           <div className="flex items-center gap-32">
             <span className="text-2xl font-light">Design & Development</span>
             <FiArrowUpRight size={40} />
           </div>
         </div>
-        <div className="flex items-center justify-between border-t border-gray-900 py-5">
+        <div className="flex items-center justify-between border-t border-gray-900 py-10">
           <h5 className="text-7xl">Rig-out</h5>
           <div className="flex items-center gap-32">
             <span className="text-2xl font-light">Design & Development</span>
             <FiArrowUpRight size={40} />
           </div>
         </div>
-        <div className="flex items-center justify-between border-y border-gray-900 py-5">
+        <div className="flex items-center justify-between border-y border-gray-900 py-10">
           <h5 className="text-7xl">Locomote</h5>
           <div className="flex items-center gap-32">
             <span className="text-2xl font-light">Design & Development</span>
             <FiArrowUpRight size={40} />
           </div>
         </div>
-        <div>
-          <button>More</button>
+        <div className="flex justify-center pt-16 ">
+          <button className="w-56 h-20 rounded-full bg-gray-900 text-white text-lg">
+            Explore more
+          </button>
         </div>
       </div>
     </section>
