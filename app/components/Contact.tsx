f11da3a47c8b72dbddc66ac074600ed536b2cbb8
dzyrick2 <daniel.gayao7@gmail.com>
2023-11-19 19:03:14 +0800

    debugged contact 2

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -17,6 +17,7 @@ export default function Contact() {
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
     requestAnimationFrame(animation); // eslint-disable-line
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   let animation; // eslint-disable-line
