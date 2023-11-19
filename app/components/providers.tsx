a2c0ae2983b8a1155e7f736e370160354aec822a
dzyrick2 <daniel.gayao7@gmail.com>
2023-11-19 20:58:19 +0800

    cursor debugging

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -4,7 +4,12 @@ import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider enableSystem={true} storageKey="theme" attribute="class">
+    <ThemeProvider
+      defaultTheme="dark"
+      enableSystem={true}
+      storageKey="theme"
+      attribute="class"
+    >
       {children}
     </ThemeProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Work from "./components/Home/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import WorkCursor from "./components/Cursor/WorkCursor";
-import Cursor from "./components/Cursor/cursor";
+import Cursor from "@/app/components/Cursor/cursor";
 
 interface MyComponentProps {
   setModal: (value: boolean) => void;
